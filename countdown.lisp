6a218d0feb06133d72aa182f2dbe774ac2fece93
Ryan Kaskel <dev@ryankaskel.com>
2012-08-06 07:54:08 +0100

    Remove package prefix.

--- countdown.lisp
+++ countdown.lisp
@@ -10,13 +10,13 @@
 (defparameter *request-url* "http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1?StopCode1=~d&ReturnList=StopPointName,DestinationText,LineName,EstimatedTime")
 
 (defun make-tfl-request (stop-id)
-  (drakma:http-request (format nil *request-url* stop-id)))
+  (http-request (format nil *request-url* stop-id)))
 
 (defun parse-tfl-json (response)
   "The TfL's API doesn't return real JSON for 'performance' reasons. We trim the response, turn it into legal JSON, then parse it."
   (decode-json-from-string
    (format nil "[~{~a~^, ~}]"
-           (cdr (cl-ppcre:split "\\n" (make-tfl-request *my-stop-id*))))))
+           (cdr (split "\\n" (make-tfl-request *my-stop-id*))))))
 
 (defun parse-tfl-response (response)
   (let ((upcoming-arrivals nil)
